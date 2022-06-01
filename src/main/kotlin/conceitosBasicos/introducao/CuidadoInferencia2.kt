ce929e9e935b54858abe4fed6c966c3fc0410de6
WybsonSantana <wybson@hotmail.com>
2022-05-31 21:53:31 -0300

    Conceitos básicos: cuidado com inferência #02

---
+++ src/main/kotlin/conceitosBasicos/introducao/CuidadoInferencia2.kt
@@ -0,0 +1,16 @@
+package conceitosBasicos.introducao
+
+fun main() {
+    var a = 17
+    var b = 11
+
+    // The floating-point literal does not conform to the expected type Int
+    // a = 1.6
+
+    var c: Double = 17.0
+    var d = 1.6
+
+    println(a + b)
+    println(c + d)
+
+}
