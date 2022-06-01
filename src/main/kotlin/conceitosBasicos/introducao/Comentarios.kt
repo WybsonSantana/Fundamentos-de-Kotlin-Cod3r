fd9dfa6f49030e8550b006c0655b2f865b3623c8
WybsonSantana <wybson@hotmail.com>
2022-05-31 21:26:34 -0300

    Conceitos básicos: comentários

---
+++ src/main/kotlin/conceitosBasicos/introducao/Comentarios.kt
@@ -0,0 +1,19 @@
+package conceitosBasicos.introducao
+
+/**
+ * Suporte ao KDoc
+ *
+ * @param args lista de parâmetros passados por linha de comando
+ */
+
+fun main() {
+    println("Hello, Kotlin!")
+    // Comentário de linha única
+
+    /*
+Comentário
+de múltiplas
+linhas
+*/
+
+}
