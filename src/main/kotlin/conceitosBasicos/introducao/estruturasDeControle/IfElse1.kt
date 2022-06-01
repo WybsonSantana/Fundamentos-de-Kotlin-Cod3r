de3f3b5bf13bf6c247209d88aa552a0ae2ddae10
WybsonSantana <wybson@hotmail.com>
2022-06-01 08:51:44 -0300

    Conceitos básicos: estrutura de controle IfElse #01

---
+++ src/main/kotlin/conceitosBasicos/introducao/estruturasDeControle/IfElse1.kt
@@ -0,0 +1,11 @@
+package conceitosBasicos.introducao.estruturasDeControle
+
+fun main() {
+    val nota = 5.3
+    if (nota >= 7) {
+        println("Aprovado!")
+    } else {
+        println("Reprovado!")
+    }
+
+}
