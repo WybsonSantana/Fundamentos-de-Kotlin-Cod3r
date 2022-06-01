763578f10f7ed7aa294f7beb3feabdfb41dfe3fb
WybsonSantana <wybson@hotmail.com>
2022-05-31 23:06:53 -0300

    Conceitos básicos: template string #01

---
+++ src/main/kotlin/conceitosBasicos/introducao/TemplateString1.kt
@@ -0,0 +1,9 @@
+package conceitosBasicos.introducao
+
+fun main() {
+    val aprovados = listOf("Zezinho", "Huguinho", "Luizinho")
+    println("O primeiro na lista de aprovados é ${aprovados[0]}")
+    println("O segundo na lista de aprovados é ${aprovados[1]}")
+    println("O terceiro na lista de aprovados é ${aprovados[2]}")
+
+}
