775c2203926350fada4a9dc14b392dc9ce1107f0
WybsonSantana <wybson@hotmail.com>
2022-06-01 08:58:10 -0300

    Conceitos básicos: estrutura de controle IfElse #02

---
+++ src/main/kotlin/conceitosBasicos/introducao/estruturasDeControle/IfElse2.kt
@@ -0,0 +1,17 @@
+package conceitosBasicos.introducao.estruturasDeControle
+
+fun main() {
+    val num1 = 17
+    val num2 = 11
+
+    val maiorValor = if (num1 > num2) {
+        println("Processando a estrutura do If...")
+        num1
+    } else {
+        println("Processando a estrutura do Else...")
+        num2
+    }
+
+    println("O maior valor entre $num1 e $num2 é $maiorValor")
+
+}
