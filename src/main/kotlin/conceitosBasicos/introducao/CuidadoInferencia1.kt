519e613c48904d42778b8c0dc06c15fba78e031f
WybsonSantana <wybson@hotmail.com>
2022-05-31 21:45:30 -0300

    Conceitos básicos: cuidado com inferência #01

---
+++ src/main/kotlin/conceitosBasicos/introducao/CuidadoInferencia1.kt
@@ -0,0 +1,14 @@
+package conceitosBasicos.introducao
+
+fun main() {
+    // This variable must either have a type annotation or be initialized
+    // var a
+
+    var a: Int
+    var b = 2
+
+    a = 15
+
+    println(a + b)
+
+}
