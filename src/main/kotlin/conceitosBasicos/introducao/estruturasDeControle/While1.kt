7175b54b75a908d3080df6b61bb15bb2eec68d20
WybsonSantana <wybson@hotmail.com>
2022-06-02 08:18:38 -0300

    Conceitos básicos: estrutura de controle While #01

---
+++ src/main/kotlin/conceitosBasicos/introducao/estruturasDeControle/While1.kt
@@ -0,0 +1,14 @@
+package conceitosBasicos.introducao.estruturasDeControle
+
+fun main() {
+    var numero: Int = 0
+
+    while (numero != -1) {
+        println("Digite um número:")
+        var input = readLine() ?: "0"
+        numero = input.toIntOrNull() ?: 0
+
+        println("Você escolheu o número $numero")
+    }
+
+}
