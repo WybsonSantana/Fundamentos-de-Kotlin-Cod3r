fbec71208e03bf52d22fb8d9c9e2d7e5afa31f66
WybsonSantana <wybson@hotmail.com>
2022-06-01 22:34:58 -0300

    Conceitos básicos: estrutura de controle When

---
+++ src/main/kotlin/conceitosBasicos/introducao/estruturasDeControle/RefatorandoParaWhen.kt
@@ -0,0 +1,25 @@
+package conceitosBasicos.introducao.estruturasDeControle
+
+fun main() {
+    val nota = 8.9
+
+    // Utilizando o operador range
+    when (nota) {
+        in 9.0..10.0 -> {
+            println("Fantástico!")
+        }
+        in 7.0..8.9 -> {
+            println("Parabéns!")
+        }
+        in 4.0..6.9 -> {
+            println("Tem como recuperar!")
+        }
+        in 0.0..3.9 -> {
+            println("Te vejo no próximo semestre!")
+        }
+        else -> {
+            println("Nota inválida!")
+        }
+    }
+
+}
