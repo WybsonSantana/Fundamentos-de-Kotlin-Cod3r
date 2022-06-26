8488cbda5a27743c3a528c49a230db1e1fcd55a4
WybsonSantana <wybson@hotmail.com>
2022-06-26 20:04:00 -0300

    Conceitos básicos: operadores lógicos

---
+++ src/main/kotlin/conceitosBasicos/introducao/operadores/Logicos.kt
@@ -0,0 +1,14 @@
+package conceitosBasicos.introducao.operadores
+
+fun main() {
+    val executouTrabalho1: Boolean = true
+    val executouTrabalho2: Boolean = false
+
+    val comprouSorvete: Boolean = executouTrabalho1 || executouTrabalho2 // Ou
+    val comprouTv50: Boolean = executouTrabalho1 && executouTrabalho2 // E
+    val comprouTv32: Boolean = executouTrabalho1 xor executouTrabalho2 // Ou exclusivo
+
+    println(comprouSorvete)
+    println(comprouTv50)
+    println(comprouTv32)
+}
