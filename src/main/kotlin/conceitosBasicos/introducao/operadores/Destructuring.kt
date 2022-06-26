74912bd6d62e3bc23ec6405c64a5ea73663b8245
WybsonSantana <wybson@hotmail.com>
2022-06-26 19:25:23 -0300

    Conceitos básicos: operador destructuring

---
+++ src/main/kotlin/conceitosBasicos/introducao/operadores/Destructuring.kt
@@ -0,0 +1,17 @@
+package conceitosBasicos.introducao.operadores
+
+data class Carro(
+    val marca: String,
+    val modelo: String
+)
+
+fun main() {
+    val (marca, modelo) = Carro("Ford", "Mustang")
+    println("$marca $modelo")
+
+    val (marido, mulher) = listOf("João", "Maria")
+    println("$marido e $mulher")
+
+    val (_, _, terceiroLugar) = listOf("Kimi", "Hamilton", "Alonso")
+    println("$terceiroLugar terminou em 3º lugar")
+}
