aa53ac4c224b3a29d60f2211e28906572f3abe7e
WybsonSantana <wybson@hotmail.com>
2022-06-26 20:13:05 -0300

    Conceitos básicos: simulando ternário #01

---
+++ src/main/kotlin/conceitosBasicos/introducao/operadores/Ternario1.kt
@@ -0,0 +1,7 @@
+package conceitosBasicos.introducao.operadores
+
+fun main() {
+    val nota: Double = 7.2
+    val resultado: String = if (nota >= 7) "Aprovado!" else "Reprovado!"
+    println(resultado)
+}
