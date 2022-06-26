495224cc60ea5a5bae1a96986a8d66aa49240a0e
WybsonSantana <wybson@hotmail.com>
2022-06-26 11:35:27 -0300

    Conceitos básicos: estrutura de controle DoWhile

---
+++ src/main/kotlin/conceitosBasicos/introducao/estruturasDeControle/DoWhile.kt
@@ -0,0 +1,12 @@
+package conceitosBasicos.introducao.estruturasDeControle
+
+fun main() {
+    var opcao: Int = -1
+
+    do {
+        val input = readLine() ?: "0"
+        opcao = input.toIntOrNull() ?: 0
+
+        println("Você escolheu a opção $opcao")
+    } while (opcao != -1)
+}
