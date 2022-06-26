66403f706d4653842ab1c4a92d56dde58492b209
WybsonSantana <wybson@hotmail.com>
2022-06-26 11:12:49 -0300

    Conceitos básicos: estrutura de controle While #02

---
+++ src/main/kotlin/conceitosBasicos/introducao/estruturasDeControle/While2.kt
@@ -0,0 +1,10 @@
+package conceitosBasicos.introducao.estruturasDeControle
+
+fun main() {
+    var contador: Int = 1
+
+    while (contador <= 10) {
+        println(contador)
+        contador++
+    }
+}
